2d9c123fe541ebe77f74694043c1880928468c56
boxnos <boxnos@yahoo.com>
2020-03-02 19:12:01 +0900

    Accept 1201A

--- 1201A/main.cpp
+++ 1201A/main.cpp
@@ -1,68 +1,69 @@
 #include <cstdio>
-#include <cctype>
 #include <utility>
-#include <vector>
+#include <cctype>
 #include <algorithm>
 using namespace std;
 
-#define _gp(l) const auto gcu{getchar##l}; const auto pcu{putchar##l}
 #ifdef __linux
-_gp(_unlocked);
+#define _U(s) s##_unlocked
 #else
-_gp();
+#define _U(s) _##s##_nolock
+#define _CRT_DISABLE_PERFCRIT_LOCKS
 #endif
+#define gcu _U(getchar)
+#define pcu _U(putchar)
 #define _DEF(r, n, ...) inline r n(__VA_ARGS__) noexcept
 #define _T template <typename T>
 #define _HT template <typename H,typename... T>
-_T _DEF(T,in,int c){T n=0;int m=1;while(isspace(c)){c=gcu();}if(c=='-')m=-1,c=gcu();
-	do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
-_T _DEF(T,in,){return in<T>(gcu());}
-_DEF(int,in,){return in<int>(gcu());}
-#define _SCAN(...) _DEF(bool,scan,__VA_ARGS__)
-_T _SCAN(T &n){int c=gcu();return c==EOF?n=0,false:(n=in<T>(c),true);}
-_SCAN(char &c){c=gcu();gcu();return c!=EOF;}
+#define _I inline
+#define _OP(t) _I operator t()
+struct _in {
 #ifdef _GLIBCXX_STRING
-_SCAN(string &s){int c;s="";
-	for(;;){c=gcu();if(c=='\n'||c==' ')return true;else if(c==EOF)return false;s+=(char)c;}}
+	_OP(string){string s;for(char c;c=gcu(),c!=' '&&c!='\n';)s+=c;return s;}
+	//_OP(string){string s;char c;while(isspace(c = gcu()));do{s+=c;}while(c=gcu(),c!=' '&&c!='\n'&&c!=EOF);return s;}
+#define _S
 #endif
+	_OP(char){char c=gcu();gcu();return c;}
+	_OP(double){double d; scanf("%lf",&d); gcu();return d;}
+	_T _OP(T){T n{},m{1},c;while(isspace(c=gcu()));if(c=='-')m=-1,c=gcu();do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
+} in;
+#define _SCAN(...) _DEF(bool,scan,__VA_ARGS__)
+#ifdef _S
+_SCAN(string &o) {int c{gcu()};if(c==EOF)return false;else{ungetc(c,stdin);string t=move(in);o=t;return true;}}
+#endif
+_T _SCAN(T &o) {int c{gcu()};return c==EOF?false:(ungetc(c,stdin),o=in,true);}
 _HT _SCAN(H &h,T&&... t){return scan(h)&&scan(t...);}
 #define _OUT(...) _DEF(void,out,__VA_ARGS__)
 #define _OUTL(...) _DEF(void,outl,__VA_ARGS__)
-_OUT(bool b){pcu('0'+b);} _OUT(const char *s){while(*s)pcu(*s++);} _OUT(char c){pcu(c);}
-#ifdef _GLIBCXX_STRING
-_OUT(string s){for(char c:s)pcu(c);}
+_OUT(bool b){pcu('0'+b);}
+_OUT(const char *s){while(*s)pcu(*s++);}
+_OUT(char c){pcu(c);}
+#ifdef _S
+_OUT(string &s){for(char c:s)pcu(c);}
 #endif
 _T _OUT(T n){static char b[20];char *p=b;T m=n<0?pcu('-'),-1:1;
 	if(!n)*p++='0';else while(n)*p++=(char)(n%10*m+'0'),n/=10;while(p!=b)pcu(*--p);}
 _OUTL(){out('\n');}
 #ifdef _GLIBCXX_VECTOR
-_T _OUT(vector<T> v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);}
+	_T _OUT(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);}
 #endif
-_HT _OUT(H&& h, T&&... t){out(h);out(move(t)...);}
-template <typename... T> _OUTL(T&&... t){out(move(t)...);outl();}
+_HT _OUT(H &&h, T... t){out(h);out(t...);}
+template <typename... T> _OUTL(T... t){out(t...);outl();}
+#define dbg(...) fprintf(stderr,__VA_ARGS__)
 struct range{
-	int e,b=0,s=1; range(int _b,int _e,int _s):e(_e),b(_b),s(_s){} range(int _b,int _e): e(_e), b(_b){} range(int _e):e(_e){}
-	struct it { int v, s; it (int _v, int _s) : v(_v), s(_s) {} operator int()const{return v;} operator int&(){return v;} int operator*()const{return v;}
-		it& operator++(){v+=s;return *this;} }; it begin() {return {b, s};} it end() {return {e, s};}};
+	int e,b=0,s=1;range(int b,int e,int s):e(e),b(b),s(s){} range(int b,int e): e(e), b(b){} range(int e):e(e){}
+	struct it{int v,s; it(int v,int s):v(v),s(s){} operator int()const{return v;} _I operator int&(){return v;}int operator*()const{return v;}
+		_I it& operator++(){v+=s;return *this;} }; it begin(){return {b,s};} it end(){return {e,s};}};
+#define times(i,n) for(int i=n;i;i--)
 
 int main() {
-	int n {in()}, m {in()};
-	vector<vector<int>> s(m, vector<int>(5));
-	for (int i = 0; i < n; i++) {
-		for (int j: range(m)) {
-			int c = gcu() - 'A';
-			s[j][c]++;
-		}
-		gcu();
+	times (i, in) {
+		int b {in}, p {in}, f {in}, h {in}, c {in};
+		if (h < c)
+			swap(h, c), swap(p, f);
+		int d {min(b / 2, p)};
+		outl(d * h + min(b / 2 - d, f) * c);
 	}
-	int r = {};
-	for (auto i: s) {
-		int m {};
-		for (int j: range(5))
-			m = max(m, i[j]);
-		r += m * in();
-	}
-	outl(r);
 }
 
 /* vim: set ts=4 noet: */
