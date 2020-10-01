e2c796e909459b54bfb85b4f09cce80de6a47205
boxnos <boxnos@yahoo.com>
2020-10-01 20:38:41 +0900

    Accept 0515A

---
+++ 0515A/main.cpp
@@ -0,0 +1,67 @@
+#pragma GCC optimize(2,3,"Ofast")
+#include <cstdio>
+#include <utility>
+#include <cctype>
+#include <cmath>
+using namespace std;
+
+#ifdef __linux
+#define _U(s) s##_unlocked
+#else
+#define _U(s) _##s##_nolock
+#define _CRT_DISABLE_PERFCRIT_LOCKS
+#endif
+#define gcu _U(getchar)
+#define pcu _U(putchar)
+#define _DEF(r, n, ...) inline r n(__VA_ARGS__) noexcept
+#define _T template <typename T>
+#define _HT template <typename H,typename... T>
+#define _I inline
+#define _OP(t) _I operator t()
+struct _in {
+#ifdef _GLIBCXX_STRING
+	_OP(string){string s;for(char c;c=gcu(),c!=' '&&c!='\n';)s+=c;return s;}
+	//_OP(string){string s;char c;while(isspace(c = gcu()));do{s+=c;}while(c=gcu(),c!=' '&&c!='\n'&&c!=EOF);return s;}
+#define _S
+#endif
+	_OP(char){char c=gcu();gcu();return c;}
+	_OP(double){double d; scanf("%lf",&d); gcu();return d;}
+	_T _OP(T){T n{},m{1},c;while(isspace(c=gcu()));if(c=='-')m=-1,c=gcu();do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
+} in;
+#define _SCAN(...) _DEF(bool,scan,__VA_ARGS__)
+#ifdef _S
+_SCAN(string &o) {int c{gcu()};if(c==EOF)return false;else{ungetc(c,stdin);string t=move(in);o=t;return true;}}
+#endif
+_T _SCAN(T &o) {int c{gcu()};return c==EOF?false:(ungetc(c,stdin),o=in,true);}
+_HT _SCAN(H &h,T&&... t){return scan(h)&&scan(t...);}
+#define _OUT(...) _DEF(void,out,__VA_ARGS__)
+#define _OUTL(...) _DEF(void,outl,__VA_ARGS__)
+_OUT(bool b){pcu('0'+b);}
+_OUT(const char *s){while(*s)pcu(*s++);}
+_OUT(char c){pcu(c);}
+#ifdef _S
+_OUT(string &s){for(char c:s)pcu(c);}
+#endif
+_T _OUT(T n){static char b[20];char *p=b;T m=n<0?pcu('-'),-1:1;
+	if(!n)*p++='0';else while(n)*p++=(char)(n%10*m+'0'),n/=10;while(p!=b)pcu(*--p);}
+_OUTL(){out('\n');}
+#ifdef _GLIBCXX_VECTOR
+	_T _OUT(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);}
+#endif
+_HT _OUT(H &&h, T... t){out(h);out(t...);}
+template <typename... T> _OUTL(T... t){out(t...);outl();}
+struct range{
+	int e,b=0,s=1;range(int b,int e,int s):e(e),b(b),s(s){} range(int b,int e): e(e), b(b){} range(int e):e(e){}
+	struct it{int v,s; it(int v,int s):v(v),s(s){} operator int()const{return v;} _I operator int&(){return v;}int operator*()const{return v;}
+		_I it& operator++(){v+=s;return *this;} }; it begin(){return {b,s};} it end(){return {e,s};}};
+#define times(i,n) for(int i=n;i;i--)
+#define dbg(...) fprintf(stderr,__VA_ARGS__)
+#define tee(s,v) ({dbg(s,v);v;})
+
+
+int main() {
+	int a {in}, b {in}, s {in}, t {s - abs(a) - abs(b)};
+	outl(t >= 0 && !(t % 2) ? "Yes" : "No");
+}
+
+/* vim: set ts=4 noet: */
