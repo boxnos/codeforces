72253bcc7e0c82551678db3312474a138482eb81
boxnos <boxnos@yahoo.com>
2020-11-05 13:24:28 +0900

    Refactor 1234B1

--- 1234B1/main.cpp
+++ 1234B1/main.cpp
@@ -71,17 +71,12 @@ struct range{
 
 // https://codeforces.com/blog/entry/62393
 struct custom_hash {
-    static size_t splitmix64(size_t x) {
-        // http://xorshift.di.unimi.it/splitmix64.c
-        x += 0x9e3779b97f4a7c15;
+    size_t operator()(size_t x) const {
+        static const size_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        x += 0x9e3779b97f4a7c15 + FIXED_RANDOM;
         x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
         x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
         return x ^ (x >> 31);
-    }
-
-    size_t operator()(size_t x) const {
-        static const size_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
-        return splitmix64(x + FIXED_RANDOM);
     }
 };
 
