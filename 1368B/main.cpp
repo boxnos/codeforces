a1f5c25680f73dacce80384e95007f6b191bfb7f
boxnos <boxnos@yahoo.com>
2021-05-29 03:24:30 +0900

    Refactor tee

--- 1368B/main.cpp
+++ 1368B/main.cpp
@@ -79,8 +79,8 @@ struct range{
 #define Range(b, ...) for([[maybe_unused]] auto b: range((int) __VA_ARGS__))
 #define dbg(...) fprintf(stderr,__VA_ARGS__)
 #define tee(s,v) ({dbg(s,v);v;})
-#define teei(v) ({dbg("%d\n",v);v;})
-#define teell(v) ({dbg("%lld\n",v);v;})
+#define teei(v) ({tee("%d\n",v);})
+#define teell(v) ({tee("%lld\n",v);})
 
 using LL = long long;
 
