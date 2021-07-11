0228ad56baa992b0862783257965efb8d2102f99
boxnos <boxnos@yahoo.com>
2021-07-11 19:40:56 +0900

    Accept 1451B

--- 1451B/main.cpp
+++ 1451B/main.cpp
@@ -1,96 +1,106 @@
+#if defined__GNUC__ && !defined __clang__
 #pragma GCC optimize("O3")
+#endif
 #include <cstdio>
 #include <utility>
 #include <cctype>
 #include <functional>
-#include <string>
-#include <vector>
 #include <array>
+
+#include <vector>
 using namespace std;
 
 #ifdef _WIN32
-#define _U(s) _##s##_nolock
+#define U_(s) _##s##_nolock
 #define _CRT_DISABLE_PERFCRIT_LOCKS
 #else
-#define _U(s) s##_unlocked
+#define U_(s) s##_unlocked
 #endif
-#define gcu _U(getchar)
-#define pcu _U(putchar)
-#define _DEF(r, n, ...) inline r n(__VA_ARGS__) noexcept
-#define _T template <typename T>
-#define _HT template <typename H,typename... T>
-#define _I inline
-#define _OP(t) _I operator t()
-struct _in {
+#define gcu U_(getchar)
+#define pcu U_(putchar)
+#define DEF_(r, n, ...) inline r n(__VA_ARGS__) noexcept
+#define T_ template
+#define TN_ typename
+#define TT_ T_ <TN_ T>
+#define HT_ T_ <TN_ H,TN_... T>
+#define I_ inline
+#define OP_(t) I_ operator t()
+struct in_ {
 #ifdef _GLIBCXX_STRING
-	_OP(string){string s;for(char c;c=gcu(),c!=' '&&c!='\n';)s+=c;return s;}
-	//_OP(string){string s;char c;while(isspace(c = gcu()));do{s+=c;}while(c=gcu(),c!=' '&&c!='\n'&&c!=EOF);return s;}
-#define _S
+	OP_(string){string s;for(char c;c=gcu(),c!=' '&&c!='\n';)s+=c;return s;}
+	//OP_(string){string s;char c;while(isspace(c = gcu()));do{s+=c;}while(c=gcu(),c!=' '&&c!='\n'&&c!=EOF);return s;}
+#define S_
 #endif
-	_OP(char){char c=gcu();gcu();return c;}
-	_OP(double){double d; scanf("%lf",&d); gcu();return d;}
-	_T _OP(T){T n{},c=gcu();do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return n;}
-	//_T _OP(T){T n{},m{1},c=gcu();if(c=='-')m=-1,c=gcu();do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
-	//_T _OP(T){T n{},m{1},c;while(isspace(c=gcu()));if(c=='-')m=-1,c=gcu();do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
+	OP_(char){char c=gcu();gcu();return c;}
+	OP_(double){double d; scanf("%lf",&d); gcu();return d;}
+	//TT_ OP_(T){T n{},c=gcu();do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return n;}
+	TT_ OP_(T){T n{},m{1},c=gcu();if(c=='-')m=-1,c=gcu();do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
+	//TT_ OP_(T){T n{},m{1},c;while(isspace(c=gcu()));if(c=='-')m=-1,c=gcu();do{n=10*n+(c-'0'),c=gcu();}while(c>='0'&&c<='9');return m*n;}
 #ifdef _GLIBCXX_VECTOR
-#define _TI template<typename T=vector<int>, typename I=typename T::value_type>
-	_TI _I T read(int n) {T v(n);for(I &i:v)i=*this;return v;}
-	_TI _I T read(int n,function<int(typename T::value_type)>f){T v(n);for(I &i:v)i=f(*this);return v;}
-	_TI _I T read(int n,function<void(typename T::value_type&,typename T::value_type)>f){T v(n);for(I &i:v)f(i,*this);return v;}
+#define TI_ T_<TN_ T=vector<int>, TN_ I=TN_ T::value_type>
+	TI_ I_ T read(int n) {T v(n);for(I &i:v)i=*this;return v;}
+	TI_ I_ T read(int n,function<int(TN_ T::value_type)>f){T v(n);for(I &i:v)i=f(*this);return v;}
+	TI_ I_ T read(int n,function<void(TN_ T::value_type&,TN_ T::value_type)>f){T v(n);for(I &i:v)f(i,*this);return v;}
 #endif
+	T_ <int N, TN_ T=int> array<T, N> read() {array<T, N> a;for (T &i: a)i=*this;return a;}
 } in;
-#define _SCAN(...) _DEF(bool,scan,__VA_ARGS__)
-#ifdef _S
-_SCAN(string &o) {int c{gcu()};if(c==EOF)return false;else{ungetc(c,stdin);string t=move(in);o=t;return true;}}
+#define SCAN_(...) DEF_(bool,scan,__VA_ARGS__)
+#ifdef S_
+SCAN_(string &o) {int c{gcu()};if(c==EOF)return false;else{ungetc(c,stdin);string t=move(in);o=t;return true;}}
 #endif
-_T _SCAN(T &o) {int c{gcu()};return c==EOF?false:(ungetc(c,stdin),o=in,true);}
-_HT _SCAN(H &h,T&&... t){return scan(h)&&scan(t...);}
-#define _OUT(...) _DEF(void,out,__VA_ARGS__)
-#define _OUTL(...) _DEF(void,outl,__VA_ARGS__)
-_OUT(bool b){pcu('0'+b);}
-_OUT(const char *s){while(*s)pcu(*s++);}
-_OUT(char c){pcu(c);}
-#ifdef _S
-_OUT(string &s){for(char c:s)pcu(c);}
+TT_ SCAN_(T &o) {int c{gcu()};return c==EOF?false:(ungetc(c,stdin),o=in,true);}
+HT_ SCAN_(H &h,T&&... t){return scan(h)&&scan(t...);}
+#define OUT_(...) DEF_(void,out,__VA_ARGS__)
+#define OUTL_(...) DEF_(void,outl,__VA_ARGS__)
+OUT_(bool b){pcu('0'+b);}
+OUT_(const char *s){while(*s)pcu(*s++);}
+OUT_(char c){pcu(c);}
+#ifdef S_
+//OUT_(string &s){for(char c:s)pcu(c);}
+OUT_(string s){for(char c:s)pcu(c);}
 #endif
-_OUT(int n) {printf("%d",n);}
-_T _OUT(T n){static char b[20];char *p=b;T m=n<0?pcu('-'),-1:1;
-	if(!n)*p++='0';else while(n)*p++=(char)(n%10*m+'0'),n/=10;while(p!=b)pcu(*--p);}
-_T _OUT(initializer_list<T> &v){for(auto i{begin(v)};i!=end(v);i++)out(i==begin(v)?"":" "),out(*i);}
+//OUT_(int n) {printf("%d",n);}
+TT_ DEF_(void,OUTX_,T n){if(n<10)pcu(n+'0');else OUTX_(n/10),pcu(n%10+'0');}
+TT_ OUT_(T n){if(n<0)pcu('-'),n=-n;OUTX_(n);}
+//TT_ OUT_(T n){static char b[20];char *p=b;T m=n<0?pcu('-'),-1:1;if(!n)*p++='0';else while(n)*p++=(char)(n%10*m+'0'),n/=10;while(p!=b)pcu(*--p);}
+TT_ OUT_(initializer_list<T> &v){for(auto i{begin(v)};i!=end(v);++i)out(i==begin(v)?"":" "),out(*i);}
 #ifdef _GLIBCXX_VECTOR
-_T _OUT(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);}
+TT_ OUT_(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);}
 #endif
-_HT _OUT(H &&h, T... t){out(h);out(t...);}
-_OUTL(){out('\n');}
-template <typename... T> _OUTL(T... t){out(t...);outl();}
-template <typename I, typename... T> _OUTL(initializer_list<I> i, T... t){out(i);outl(t...);}
-template <typename T=int>
-struct range{
+HT_ OUT_(H &&h, T... t){out(h);out(t...);}
+OUTL_(){out('\n');}
+T_ <TN_... T> OUTL_(T... t){out(t...);outl();}
+T_ <TN_ I, TN_... T> OUTL_(initializer_list<I> i, T... t){out(i);outl(t...);}
+T_ <TN_ T=int> struct range{
 	T e,b=0,s=1;range(T b,T e,T s):e(e),b(b),s(s){} range(T b,T e): e(e), b(b){} range(T e):e(e){}
-	struct it{T v,s; it(T v,T s):v(v),s(s){} operator T()const{return v;} _I operator T&(){return v;}T operator*()const{return v;}
-		_I it& operator++(){v+=s;return *this;}}; it begin(){return {b,s};} it end(){return {e,s};}};
+	struct it{T v,s; it(T v,T s):v(v),s(s){} operator T()const{return v;} I_ operator T&(){return v;}T operator*()const{return v;}
+		I_ it& operator++(){v+=s;return *this;}}; it begin(){return {b,s};} it end(){return {e,s};}};
 #define Range(b, ...) for([[maybe_unused]] auto b: range((int) __VA_ARGS__))
 #define dbg(...) fprintf(stderr,__VA_ARGS__)
+using LL = long long;
 #define tee(s,v) ({dbg(s,v);v;})
+#define TAPP(t, s) t tapp(t v) {return tee(s, v);}
+TAPP(char, "%c") TAPP(int, "%d") TAPP(LL, "%lld")
+#ifdef S_
+string tapp(string s) {return tee("%s", s.c_str());}
+#endif
+TT_ T tapp(T v){for (auto i: v){tapp(i);dbg(" ");}return v;}
+TT_ T tapl(T v){tapp(v);dbg("\n");return v;}
+
+LL fix(LL a, LL b) {
+	return a > 0 ? a : a + b;
+}
 
 int main() {
 	Range (t, in) {
-		int n {in}, q {in};
-		string s = in;
-		vector<bool> f(n), b(n);
-		auto set = [&](auto &f, int a, int b, int c) {
-			array<bool, 2> F {};
-			Range (i, a, b, c)
-				f[i] = exchange(F[s[i] - '0'], true);
-		};
-		set(f, 0, n, 1);
-		set(b, n - 1, -1, -1);
-		Range (i, q) {
-			int l {(int) in - 1}, r {(int) in - 1};
-			outl(f[l] || b[r] ? "YES" : "NO");
-		}
+		int n {in}, r {};
+		auto a = in.read<vector<LL>>(n);
+		Range (i, n - 1)
+			for (int j = i + fix(a[i] - (i + 1) % a[i] * 2, a[i]); j < n; j += a[i])
+				if (a[i] * a[j] == i + j + 2)
+					r++;
+		outl(r);
 	}
 }
-
 
 /* vim: set ts=4 noet: */
