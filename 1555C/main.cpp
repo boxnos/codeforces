e5aafe77fd948f97d276d1b8b8ef5e685071c444
boxnos <boxnos@yahoo.com>
2021-11-15 23:29:05 +0900

    Optimize 1555C

--- 1555C/main.cpp
+++ 1555C/main.cpp
@@ -66,7 +66,7 @@ TT_ DEF_(void,OUTX_,T n){if(n<10)pcu(n+'0');else OUTX_(n/10),pcu(n%10+'0');}
 TT_ OUT_(T n){if(n<0)pcu('-'),n=-n;OUTX_(n);}
 TT_ OUT_(initializer_list<T> v){for(auto i{begin(v)};i!=end(v);++i)out(i==begin(v)?"":" "),out(*i);}
 #ifdef _GLIBCXX_VECTOR
-TT_ OUT_(vector<T> &v){for(T &x:v)out(&x == &v[0]?"":" "),out(x);}
+TT_ OUT_(vector<T> &v){if(auto i{begin(v)},e{end(v)};i!=e){out(*i++);for(;i!=e;++i)out(' '),out(*i);}}
 #endif
 HT_ OUT_(H &&h,T... t){out(h);out(t...);}
 OUTL_(){out('\n');}
